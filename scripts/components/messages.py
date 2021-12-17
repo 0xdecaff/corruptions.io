1880f243417d6d9a7cbf87632e41cd1a1ebd6e02
0xdecaff <0xdecaff@protonmail.com>
2021-12-16 21:19:39 -0700

    :pencil: add prologue section to messages

--- scripts/components/messages.py
+++ scripts/components/messages.py
@@ -11,24 +11,13 @@ class Message(pydantic.BaseModel):
     createdAt: datetime.datetime
     channel: str
     message: str
-    b64: bool = False
-
-    @classmethod
-    def from_json(cls, payload: dict):
-        m = Message(**payload)
-        contains_b64 = False
-        message = m.message.split(" ")
-        for word in message:
-            if (len(word) > 5 and word.endswith("=")) or len(word) > 30:
-                contains_b64 = True
-        m.b64 = contains_b64
-        return m
+    chapter: str = ""
 
 
 def download() -> None:
     """Fetch & save json for all on-chain messages"""
 
-    # TODO: read existing messages from disk, get only latest messages, and update existing data
+    # TODO: get only latest messages
     query = """
     {
     messages(first: 250) {
@@ -39,14 +28,22 @@ def download() -> None:
     }
     }
     """
+    # Get existing messages, and only add the new ones
+    messages_filepath = Path(__file__).parent.parent.parent / "site/data/messages.json"
+    with open(messages_filepath, mode="r") as f:
+        messages = json.load(f)
+        messages = messages["messages"]
+
+    existing_messages = {k.get("id") for k in messages}
+
     request = requests.post(
         "https://api.thegraph.com/subgraphs/name/shahruz/corruptions",
         json={"query": query},
     )
     request.raise_for_status()
-    messages = request.json().get("data", {}).get("messages", [])
-    messages = sorted(messages, key=lambda x: x.get("createdAt"))
-    messages = [Message.from_json(x).dict() for x in messages]
-    messages_filepath = Path(__file__).parent.parent.parent / "site/data/messages.json"
+    # TODO: sort in db
+    new_messages = request.json().get("data", {}).get("messages", [])
+    new_messages = sorted(new_messages, key=lambda x: x.get("createdAt"))
+    new_messages = [Message(**x).dict() for x in new_messages if x.get("id") not in existing_messages]
     with open(messages_filepath, mode="w") as f:
-        json.dump({"messages": messages}, f, indent=2, default=str)
+        json.dump({"messages": messages + new_messages}, f, indent=2, default=str)
